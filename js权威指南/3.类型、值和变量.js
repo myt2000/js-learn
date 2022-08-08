fb293dc59b7b82c5cd6be288337281652ce2417a
brooks <myt2000@126.com>
2022-08-08 16:46:26 +0800

    js权威指南的作用域的学习

---
+++ js权威指南/3.类型、值和变量.js
@@ -0,0 +1,68 @@
+// console.log(({ x: 1, y: 2 }).toString())    // [object Object]
+
+// console.log([1, 2, 3].toString())       // 1,2,3
+// console.log((function (x) { f(x) }).toString())     // function (x) { f(x) }
+// console.log((new Date(2022, 8, 5)).toString())      // Mon Sep 05 2022 00:00:00 GMT+0800 (中国标准时间)
+// console.log(/\d+/g.toString())      // /\d+/g
+
+
+// console.log([1, 2, 3].valueOf())       // [ 1, 2, 3 ]
+// console.log((function (x) { f(x) }).valueOf())     // [Function (anonymous)]
+// console.log((new Date(2022, 8, 5)).valueOf())      // 1662307200000
+// console.log(/\d+/g.valueOf())      // /\d+/g
+
+
+// console.log([0].toString()) // 0
+// console.log([0].valueOf())  // [ 0 ]
+
+// console.log(1 + [1])         // 11
+// console.log(1 + 1)           // 2
+// console.log(1 + '1')        // 11
+
+// var now = new Date()
+// console.log(typeof (now + 1))       // string
+// console.log(typeof (now - 1))       // number
+// console.log(now == now.toString())  // true
+// console.log(now > (now - 1))        // true
+
+// var i;
+// var sum;
+
+// var i, sum;
+
+
+// var message = "hello";
+// var i = 0, j = 0, k = 0;
+
+
+// for (var i = 0; i < 10; i++) console.log(i);
+// for (var i = 0, j = 10; i < 10; i++, j--) console.log(i * j);
+// for (var p in o) console.log(p);
+
+
+// var scope = "global";
+// function checkscope() {
+//     var scope = "loacal";
+//     return scope;
+// }
+// console.log(checkscope())   // loacal
+
+// scope = "global";
+// function checkscope2() {
+//     scope = "local";
+//     myscope = "local";
+//     return [scope, myscope];
+// }
+
+// console.log(checkscope2)
+
+// var scope = "global scope";
+// function checkscope() {
+//     var scope = "local scope";
+//     function nested() {
+//         return scope;
+//     }
+//     return nested();
+// }
+
+// console.log(checkscope())       // local scope
